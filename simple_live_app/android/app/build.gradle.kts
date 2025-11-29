a98e27d1e892d3ed32ee6a068ba23173c315dc4c
thoryoungst-boop <thoryoungst@gmail.com>
2025-11-29 18:49:01 +0100

    Update build.gradle.kts

--- simple_live_app/android/app/build.gradle.kts
+++ simple_live_app/android/app/build.gradle.kts
@@ -1,11 +1,25 @@
-val keystoreProperties = Properties()
+import java.util.Properties
+import java.io.FileInputStream
+
 val keyPropsFile = file("key.properties")
+val keystoreProperties = Properties()
 
 if (keyPropsFile.exists()) {
     keystoreProperties.load(FileInputStream(keyPropsFile))
 }
 
 android {
+    namespace = "com.example.simple_live_app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.simple_live_app"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
     signingConfigs {
         create("release") {
             if (keyPropsFile.exists()) {
@@ -14,7 +28,7 @@ android {
                 keyAlias = keystoreProperties["keyAlias"] as String
                 keyPassword = keystoreProperties["keyPassword"] as String
             } else {
-                // 无签名 APK
+                // CI 无签名 APK
                 enableV1Signing = false
                 enableV2Signing = false
             }
@@ -25,6 +39,7 @@ android {
         getByName("release") {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
 }
